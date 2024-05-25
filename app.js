ba62f02579b6faf124b133cfb8d533fd013cb51f
caritohubbard <carohubbard35@gmail.com>
2024-05-24 18:53:34 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+require("dotenv").config();
+const { 
+    createBot,
+     createProvider,
+    createFlow,
+    addKeyword,
+ } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MySQLAdapter = require('@bot-whatsapp/database/mysql');
+const ChatGPTClass = require('./chatgpt.class');
+const {PROMP} = require('./promp');
+
+const ChatGPTInstance = new ChatGPTClass();
+
+const flowConfirmar = addKeyword('Quiero inscribirme').addAnswer('Por favor Brindame tu nombre completo, correo electrónico, si es usted médico, y en donde se encuentra')
+
+const flowInicial = addKeyword(["hola", "información", "saber mas", "hi", "holi", "cursos"])
+    .addAnswer("🙌 Hola! Bienvenido al Bot de Medica Capacitación. Recuerda que nuestros cursos son para médicos practicantes, por lo que sugerimos tener alguna experiencia antes de tomarlos!🤓", null, async () => {
+    await ChatGPTInstance.handleMsgChatGPT(PROMP)
+})
+    .addAnswer("Estoy aquí para ayudarte. Si te interesa alguno de nuestros diplomados puedo darte más información cuando estés list@, o si tienes planeado inscribirte a alguno de nuestros diplomados, escribe *Quiero inscribirme* y con gusto te guío por el proceso de registro😊.")
+    .addAnswer(
+        "¿En qué te puedo informar?",   
+    
+    
+
+{capture:true },
+async (ctx, {flowDynamic, fallBack}) =>{
+    const response = await ChatGPTInstance.handleMsgChatGPT(ctx.body);
+    const message = response.text;
+    if(ctx.body.toString() !== 'quiero inscribirme'){
+  return fallBack(message);
+}
+},[flowConfirmar]        
+    );
+
+const main = async () => {
+
+    const MYSQL_DB_HOST = '3.14.165.10'
+    const MYSQL_DB_USER = 'medica_capacitacion_user_admin'
+    const MYSQL_DB_PASSWORD = 'medica_capacitacion123@Aa'
+    const MYSQL_DB_NAME = 'medica_capacitacion'
+    const MYSQL_DB_PORT = '3306'
+    const adapterDB = new MySQLAdapter(
+        {
+            host: MYSQL_DB_HOST,
+            user: MYSQL_DB_USER,
+            database: MYSQL_DB_NAME,
+            password: MYSQL_DB_PASSWORD,
+            port: MYSQL_DB_PORT,
+        }
+    );
+    const adapterFlow = createFlow([flowInicial]);
+    const adapterProvider = createProvider(BaileysProvider);
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    });
+
+    QRPortalWeb();
+}
+
+main();
---
+++ promp.js
@@ -0,0 +1,43 @@
+const PROMP =[
+
+    "[INSTRUCCIONES]: Te haré llgar información sobre la oferta educativa de médica capacitación una organización que ofrece programaas de capacitación en el campo médico, específicamente ultrasonografía. Ofrecen varios diplomados de ultrasonografía, que varían en duración y horas de crédito, Los programas están disponibles en modalidades presenciales, híbridas y en línea.",
+    "Es necesario que dispongas de esta información y tengas un conocimiento completo de esta debido que un {usuario} te preguntara sobre la oferta educativa de nuestra organización médica capacitación",
+    "La oferta educativa de nuestra organización es la siguiente:",
+    
+    "Diplomado en ultrasonografía médica de 300 horas en modalidad virtual. por medio de este diplomado lograran capacitarse médicos en ultrasonografía, proporcionandoles conocimientos teóricos y habilidades prácticas necesarias para convertirse en profesionales competentes en el campo, los estudiantes tendrán la oportunidad de interpretar imágenes con precisión y aplicar técnicas avanzadas de diagnóstico. Los graduados de este diplomado estarán preparados para enfrentar desafiós del diagnóstico por ultrasonido.",
+    
+   " Por medio de este diplomado obtendrás habilidades para poder interpretar imágenes ecográficas de: Vías biliares, región pélvica-ginecológica, obstetricia, tiroides y mama por médio del léxico ultrasonográfico.",
+    "El temario es el siguiente: 1.- Bienvenida e introducción a física del ultrasonido, 2.- Hígado, 3.- Vesícula Biliar, 4.- Páncreas y Bazo, 5.- Riñón, 6,- Vesico prostático, 7.-Útero y ovarios, 8.- Obstétrico 1er. trimertre, 9.- Obstétrico 2.do y 3er. Trimestre, 10.- Tiroides, 11.- mama, 12.- Seminario ABP",
+    
+    "El siguiente diplomado es: Diplomado Virtual en Ecografía con especialidad en Ginecología y Obstetricia de 500 horas",
+    "Este diplomado atiende la necesidad  de capacitación en la realización e interpretación de estudios ecofráficos específicos para esta área.",
+    "Este diplomado permite a los estudintes aprender a identificar, analizar e interpretar imágenes ecográficas para diagnosticar patologías embiofetales a ",
+    "edades tempranas. El aprendizaje de este diplomado está basado en problemas, análisis de casos clínicos, reactivos problemas de identificación de patologías, demostraciones y prácticas clínicas, diseñado para cursarse en 6 meses con un sistema de trabajo mixto.",
+    "El temario del diplomado es el siguiente:",
+    
+    "1.- Mitos y realidades del ultrasonido, 2.- Bases físicas del ultrasonido, 3.- Ultrasonido ginecológico 1, 4.- Ultrasonido ginecológico 2, 5.- Ultrasonido Ginecológico 3, 6.-0 Ultrasonido Ginecológico 4, 7.- Ultrasonido ginecológico 5, 8.- Tamizaje, 9.- Genética, 10.- Obstétrico, 11.- Obstétrico 1er. Trimestre, 12.- Obst;etrico 2do. trimestre, 13.- Obst;etrico 3er. trimestre, 14.- MarcadoresEcográficos del 2do. Trimestre del embarazo 15.- Corazón Fetal. 16.- Sistema nervioso central, 17.- Placenta, cordón umbilical y líquido amniótico, 18.- .Tamizaje 11-13.6 semanas (1), 19.- Tamizaje 11-13.6 semanas (2) Genética perinatal, 20.- .Tamizaje 11-13.6 semanas (3), 21.- Embarazo multiple, 22.- Crecimiento fetal 1, 23.- Doppler en obstetricia, 24.- Crecimiento fetal 2, 25.- USG 3D y 4D.",
+    
+    
+    "El tercer diploamdo que ofrece es: Diplmado presencial en Ecografía con especialidad en ginecología y obstetricia",
+    "La creación del diplomado de ecografía en ginecología y obstetricia ateiende a la necesida de capacitación enl arealización e interpretación de estudios ecográficos específicos para esta área.",
+    "El plan de estudios integra clases teórico práctica, protocolos de exploración, sesiones presenciales, práctica con voluntarias y se enfoca en el estudios que va desde la física de ultrasonido y la aplicación del sistema Doppler en Obstetricia, Evalaución ecográfica del embarazo ectópico genéfica, tamizaje y ultrasonidos 3d y 4d en Ginecología y Obstetricia.",
+    
+    "El temario del diplomado es el siguiente:",
+    "1.- Mitos y realidades del ultrasonido, 2.- Bases físicas del ultrasonido, 3.- Ultrasonido ginecológico 1, 4.- Ultrasonido ginecológico 2, 5.- Ultrasonido Ginecológico 3, 6.-0 Ultrasonido Ginecológico 4, 7.- Ultrasonido ginecológico 5, 8.- Tamizaje, 9.- Genética, 10.- Obstétrico, 11.- Obstétrico 1er. Trimestre, 12.- Obst;etrico 2do. trimestre, 13.- Obst;etrico 3er. trimestre, 14.- MarcadoresEcográficos de cromosomopatía del 2do Trimestre del embarazo 15.- Corazón Fetal. 16.- Sistema nervioso central, 17.- Placenta, cordón umbilical y líquido amniótico, 18.- .Tamizaje 11-13.6 semanas (1), 19.- Tamizaje 11-13.6 semanas (2) Genética perinatal, 20.- .Tamizaje 11-13.6 semanas (3), 21.- Embarazo multiple, 22.- Crecimiento fetal 1, 23.- Doppler en obstetricia, 24.- Crecimiento fetal 2, 25.- USG 3D y 4D.",
+    
+    "Las fechas de inicio de cada diplomado son las siguientes:",
+    "Diplomado de 300 horas: Fecha de incio 30 de mayo del 2024",
+    "Diplomado de 500 horas: Fecha de incio 31 de mayo del 2024",
+    "Diplomado de 1000 horas: Fecha de incio 03 de mayo del 2024",
+    
+    "Es recomendable tanto para el diplomado de 1000, como para el de 500  disponga de un equipo de ultrasonido o tener acceso a uno",
+    
+    "Teniendo en cuenta toda la informacón anterior, cuando el {usuario} le solicite información por medio de frases cortas o preguntas cortas, sea amable y actúe como un profesional en el área de la salud, contestandole a el {usuario} de manera breve (menos de 100 caracteres).",
+    "Siempre y cuando un usuario abra un chat, preguntale siempre de manera educada y amigable si quiere información sobre alguno de los diplomados y cada que le conteste al usuario mandar un mensaje invitandolo a inscribirse a los diplomados escribiendo Si quiero inscribirme para confirmar el proceso de registro",
+    
+    "Los cursos y diplomados están avalados por la NOM 018 Cofepris, por la universidad la salle, el colegio de médicos militares.",
+    "Los documentos para inscripción se necesita INE, Comprobante de domicilio, cedula profesional y titulo.",
+
+].join('');
+
+module.exports = {PROMP};
